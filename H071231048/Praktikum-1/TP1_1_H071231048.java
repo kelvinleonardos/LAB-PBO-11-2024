19c272dd9c4967fd4a0fddc0b00e252b49910add
andiaisar <andiaisar42@gmail.com>
2024-03-23 22:39:57 +0800

    Kumpul TP1

---
+++ H071231048/Praktikum-1/TP1_1_H071231048.java
@@ -0,0 +1,37 @@
+package Tugas_Praktikum.Praktikum_1;
+import java.util.Scanner;
+
+public class TP1_1_H071231048 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int bilanganbulat = 0;
+        int bilangandesimal = 0;
+
+        String inputX = scanner.nextLine(); 
+
+        try {
+            
+            int x = Integer.parseInt(inputX);
+
+            for (int i = 0; i < x; i++) {
+                if (scanner.hasNextInt()) {
+                    scanner.nextInt();
+                    bilanganbulat++;
+                } else if (scanner.hasNextDouble()) {
+                    scanner.nextDouble();
+                    bilangandesimal++;
+                } else {
+                    System.out.println("Index ke-" + i + ": Input bukan angka");
+                    scanner.next(); 
+                }
+            }
+        } catch (NumberFormatException e) {
+               
+            System.out.println("Input awal bukan bilangan bulat: " + inputX);
+        }
+
+        System.out.println(bilanganbulat + " Bilangan Bulat");
+        System.out.println(bilangandesimal + " Bilangan Desimal");
+        scanner.close();
+    }
+}
