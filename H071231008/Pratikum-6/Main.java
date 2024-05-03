55c86dda1c894db1bc7ef28578f29c958c52dc14
AndiRiswanda <andiriswandalah@gmail.com>
2024-05-03 16:00:29 +0800

    menambahkan tugas pratikum 6

---
+++ H071231008/Pratikum-6/Main.java
@@ -0,0 +1,22 @@
+package Week6;
+
+public class Main {
+    public static void main(String[] args) {
+        
+      
+        Pitbul anjing1 = new Pitbul();
+
+        anjing1.Move();
+        anjing1.describe();
+        System.out.println();
+
+        Smartphone Smartphone = new Smartphone(12000000, "Iphone");
+        Smartphone.Move();
+
+        Car Car1 = new Car(12, "Merah", 100);
+
+        Car1.Move();
+        
+    }
+    
+}
