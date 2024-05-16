93c1929bb5dc2c11485c1c46dc04bbe6d2bf1827
NurulWahdania <nurulwahdania182@gmail.com>
2024-05-16 14:55:46 +0800

    TP8

---
+++ H071231005/Praktikum-8/TP8_2_H071231005/Result.java
@@ -0,0 +1,25 @@
+public class Result {
+    private String name;
+    private int finishtime;
+
+    public Result(String name, int finishtime){
+        this.name = name;
+        this.finishtime = finishtime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setBotName(String racerName){
+        this.name = racerName;
+    }
+
+    public int getFinishTime() {
+        return finishtime;
+    }
+
+    public void setFinishTime(int racerTime) {
+        this.finishtime = racerTime;
+    }
+}
